b61ce3bc8ab1e1f0c1ea980db8e9e5d395b3f46d
Emile Swain <Emile.Swain@akqa.com>
2015-04-28 15:32:05 +0100

    PreviewCameraRig added

---
+++ Assets/AKQA/behaviours/CameraPreviewRig.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraPreviewRig : MonoBehaviour {
+
+
+
+	public GameObject yaw;
+	public GameObject pitch;
+	public GameObject tilt;
+
+	float sensitivity = 2f;
+
+	void Start () {
+
+	}
+	
+
+	void Update () {
+		if (Input.GetAxis("Horizontal") >0 || Input.GetAxis("Horizontal") <0 )
+		{
+			float rotationX = Input.GetAxis("Horizontal") * sensitivity;
+			yaw.transform.Rotate(0, rotationX, 0);
+		}
+		if (Input.GetAxis("Vertical") >0 || Input.GetAxis("Vertical") <0 )
+		{
+			float rotationY = Input.GetAxis("Vertical") * sensitivity;
+			pitch.transform.Rotate(-rotationY,0, 0);
+		}
+	}
+
+
+}
